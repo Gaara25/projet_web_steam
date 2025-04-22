2ce9402202465213111cea887a80186900fc2e29
Gaara25 <67744708+Gaara25@users.noreply.github.com>
2025-04-22 19:14:42 +0200

    WIP: translation & visual for user

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class DefaultController extends AbstractController
 {
@@ -13,6 +15,20 @@ final class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig', [
 
+        ]);
+    }
+
+    #[Route('/steam/{id}', name: 'steam_profile', defaults: ['id' => 1])]
+    public function profile(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        return $this->render('steam/profile.html.twig', [
+            'user' => $user,
         ]);
     }
 }
